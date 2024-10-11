931105af04e9a51f0b3fcf6509b6d974fb0be387
yongdam4 <kydkyd7194@naver.com>
2024-10-11 17:41:24 +0900

    커밋

--- java/com/semi/product/service/ProductService.java
+++ java/com/semi/product/service/ProductService.java
@@ -1,9 +1,5 @@
 package com.semi.product.service;
 
-<<<<<<< HEAD
-public interface ProductService {
-	
-=======
 import java.util.List;
 
 import com.semi.product.model.vo.Product;
@@ -12,5 +8,4 @@ public interface ProductService {
 	public List<Product> getAllProducts();  // 상품 목록 조회 메소드
 	public List<Product> searchProductsByName(String name);  
     public List<Product> searchProductsByCateNum(String catenum);   
->>>>>>> 2b354c79c1fbfa98682f66c02f377035d25672e2
 }
